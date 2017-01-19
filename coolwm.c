f6cf51015331df78c9034d7f6afd042d5484b8de
justin <justinledford@gmail.com>
2017-01-18 22:03:35 -0600

    move cursor with window

--- coolwm.c
+++ coolwm.c
@@ -13,6 +13,7 @@ void move_window(int x, int y)
     if (ev.xkey.subwindow != None) {
         XGetWindowAttributes(dpy, ev.xkey.subwindow, &attr);
         XMoveWindow(dpy, ev.xkey.subwindow, attr.x + x, attr.y + y);
+        XWarpPointer(dpy, None, ev.xkey.subwindow, 0, 0, 0, 0, 0, 0);
     }
 }
 
